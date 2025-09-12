a16cec05084a7a432ee2968b3eb58729b50c3423
unknown <mayankdaryani001@gmail.com>
2025-09-12 14:59:20 +0530

    initial commit

---
+++ Backend/src/db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+
+
+function connectDB() {
+    mongoose.connect("mongodb://localhost:27017/food-view")
+    .then(()=>{
+        console.log("database connected");
+
+    })
+    .catch((err)=>{
+        console.log("MongoDB connection error:",err);
+    })
+}
+
+
+module.exports = connectDB;
