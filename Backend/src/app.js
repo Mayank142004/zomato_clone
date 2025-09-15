2d6e726ab3bdd0c5030bdf90f96fa9b83c6c0be7
unknown <mayankdaryani001@gmail.com>
2025-09-15 11:37:20 +0530

    1.1

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,13 @@
 // create server
 const express= require('express')
 
+const cookieParser = require('cookie-parser');
+
+
 
 const app = express();
+app.use(express.json()); // it's a middleware use for req.body in controller
+app.use(cookieParser());
 
 
 app.get("/", (req, res) =>{
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const { route } = require('../app');
+const authController = require('../controllers/auth.controller');
 
 const router = express.Router;
 
+route.post('/user/register',authController.registerUser)
+route.post('/user/register',authController.loginUser)
 
 
 module.exports= router;
